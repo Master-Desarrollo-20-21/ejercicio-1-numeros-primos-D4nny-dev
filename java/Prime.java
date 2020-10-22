92b88b1f6104afb88fdced155d13225dbeff6bf6
Dany <danielgarciabrito@gmail.com>
2020-10-22 21:21:45 +0100

    Fix primes exercise

--- java/Prime.java
+++ java/Prime.java
@@ -1,8 +1,8 @@
 class Prime {
 	
 	public static  boolean isPrime(int n) {
-		
-		if (n == 1) return false;
+
+		if (n < 2) return false;
 		for( int i = 2; i < n; i++ ) {
         	if( n % i == 0)
             	return false;
@@ -26,21 +26,19 @@ class Prime {
 		int num = 0;
 		int sum = 0; 
 		
-		for(int i = 0 ; i <= 50;){
+		for(int i = 0 ; i < n;){
 			if(isPrime(num)){
 				sum += num;
-				num++;
 				i++;		
-			}else{
-				num++;
 			}
+		    num++;
 		}
 		return sum;
 	}
 
 	public static void main(String[] args){	
 		System.out.println("Add the first primes in rage of [0 - 50]: " + addFirstFiftyPrimes());
-		System.out.println("Add of the first 50's primes is: " + addFirstNprimes(50));
+		System.out.println("Add of the first 50's primes is: " + addFirstNprimes(50));		
 	}
 }
 
